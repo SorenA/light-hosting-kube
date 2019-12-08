5fdec7003ad2eb2b5739e227aa44b519a9040040
Lukas Joergensen <lpjoergensen@gmail.com>
2019-12-08 02:34:43 +0100

    Create variables.tf

---
+++ terraform/modules/dns/digitalocean/variables.tf
@@ -0,0 +1,11 @@
+# Cluster configuration
+
+variable "cluster_domain" {}
+variable "cluster_enable_floating_ip" {}
+variable "servers" {}
+variable "server_ips" {}
+variable "floating_ip" {}
+
+# Provider configuration - Digital Ocean
+
+variable "digitalocean_token" {}
